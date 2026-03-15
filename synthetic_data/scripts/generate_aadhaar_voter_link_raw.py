56dca06b6bb69d7c08637246a7b7fb84fbc7f70e
atul_bhardwaj <atulbhardwajdinesh676@gmail.com>
2026-03-16 02:29:47 +0530

    mata data add

--- synthetic_data/scripts/generate_aadhaar_voter_link_raw.py
+++ synthetic_data/scripts/generate_aadhaar_voter_link_raw.py
@@ -64,13 +64,15 @@ columns = [
     'linkage_pct',
     'kyc_pending',
     'duplicate_suspected',
-    'data_source',
+    'source_system',
     'ingest_ts',
+    'batch_id',
 ]
 
 location_count = len(locations)
 month_count = len(months)
 ingest_ts = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+batch_id = datetime.now(timezone.utc).strftime('aadhaar_voter_link_%Y%m%dT%H%M%SZ')
 
 with out_file.open('w', newline='', encoding='utf-8') as f:
     writer = csv.writer(f)
@@ -109,6 +111,7 @@ with out_file.open('w', newline='', encoding='utf-8') as f:
             duplicate_suspected,
             'government_website',
             ingest_ts,
+            batch_id,
         ])
 
         if (i + 1) % 200000 == 0:
